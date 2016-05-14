3ba888174da2dc2669e4cae50cea0e9d1578169a
Dom Williams <me@domwillia.ms>
2016-05-14 18:22:21 +0100

    Add portable ntoh

--- shared/include/shared_utils.h
+++ shared/include/shared_utils.h
@@ -3,6 +3,20 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+// Portable 64 bit host-to-network conversion
+// Merci http://stackoverflow.com/a/4410728
+#if defined(__linux__)
+#  include <endian.h>
+#elif defined(__FreeBSD__) || defined(__NetBSD__)
+#  include <sys/endian.h>
+#elif defined(__OpenBSD__)
+#  include <sys/types.h>
+#  define be16toh(x) betoh16(x)
+#  define be32toh(x) betoh32(x)
+#  define be64toh(x) betoh64(x)
+#endif
 
 // Prints the given error message to stderr
 #define error_print(msg) fprintf(stderr, "ERROR: %s", msg);
